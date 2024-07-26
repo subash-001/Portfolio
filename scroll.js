4bc30caf447549842d5a582efd46716d9c3d97eb
Subash <subashs10011@gmail.com>
2024-07-26 12:17:22 +0530

    about

--- scroll.js
+++ scroll.js
@@ -30,4 +30,16 @@ window.onload = function() {
     });
   };
 
-//   nav bar
+//   scroll
+const observer= new IntersectionObserver((entries) =>{
+  entries.forEach((entry)=>{
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const hidden = document.querySelectorAll('.bgcolor');
+hidden.forEach((el) => observer.observe(el));
